55634f3923cfb807cbd5b49df0dd875cafbee7e6
Suresh Padmanabhan <rathna.ganjigunta@gmail.com>
2016-02-25 20:11:51 -0800

    Particle waves

---
+++ Day_32/particle_waves.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	 var numberOfCards = _.range(1000);
+
+	 _.each(numberOfCards, function(position){
+	 	$('<div>', {
+	 		id: 'card' + position,
+	 		class: 'card'
+	 	}).on('click',function(event){
+	 		$(this).off('click');
+	 		cascade(event, position);
+	 	}).appendTo('#container');
+	 });
+     
+	 $('#container').append('<br class="clear">');
+
+	 function cascade (event, position){
+	 	var pos = position;
+
+	 	flip(pos);
+
+	 	setTimeout(function(){
+           		$('#card'+ (pos + 1)).trigger('click');
+           		$('#card'+ (pos - 1)).trigger('click');
+           		$('#card'+ (pos - 40)).trigger('click');
+           		$('#card'+ (pos + 40)).trigger('click');
+
+	 	}, 50);
+
+	 	setTimeout(function(){
+	 		$('#card'+ pos).on('click',function(event){
+	 			$(this).off('click');
+	 			cascade(event, position);
+	 		});
+	 	}, 800);
+	 };
+
+	 function flip(id){
+	 	var cardID = '#card'+ id;
+	 	 	$(cardID).addClass('wave');
+	 	 	setTimeout(function(){
+	 	 		$(cardID).removeClass('wave');
+
+	 	 	},400);	 
+	 	 }
+
+
+});
